aa7f9d175d9afb4280fa0dfa4a3f60357889ef36
mahalakshmi <mahalakshmidokkada@gmail.com>
2025-04-02 07:52:41 +0530

    shape

---
+++ shape/Circle.java
@@ -0,0 +1,16 @@
+package test2;
+
+class Circle extends Shape {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+    @Override
+    double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ shape/Shape.java
@@ -0,0 +1,6 @@
+package test2;
+
+public abstract class Shape {
+	abstract double calculateArea();
+    abstract double calculatePerimeter();
+}
